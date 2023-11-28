da405ed7fea32473aa048fab3a77881dc2ec45e7
pleasebelieveme <146408987+pleasebelieveme@users.noreply.github.com>
2023-11-28 14:21:31 +0900

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,33 +1,45 @@
-# AWS Provider 설정
+# AWS 서비스 및 지역 설정 (서울 지역)
 provider "aws" {
-  region = "ap-northeast-2"  # AWS 리전을 대한민국 서울로 설정
+    region = "ap-northeast-2"
 }
 
 # VPC 리소스 생성
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"  # VPC의 IP 주소 범위 설정
-  tags = {
-    Name = "myVPC"  # 리소스에 대한 태그 설정
-  }
+    cidr_block = "172.16.0.0/16" # VPC의 IP 주소 범위 설정
+    tags = {
+        Name = "myVPC" # VPC에 이름 설정
+    }
 }
 
 # Subnet 리소스 생성
 resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id  # VPC ID를 참조하여 Subnet을 해당 VPC에 연결
-  cidr_block        = "172.16.10.0/24"  # Subnet의 IP 주소 범위 설정
-  availability_zone = "ap-northeast-2a"  # Subnet이 위치할 가용 영역 설정
-  tags = {
-    Name = "mySubnet"  # 리소스에 대한 태그 설정
-  }
+    vpc_id            = aws_vpc.my_vpc.id
+    cidr_block        = "172.16.10.0/24" # Subnet의 IP 주소 범위 설정
+    availability_zone = "ap-northeast-2a" # 사용할 가용 영역 설정
+    tags = {
+        Name = "mySubnet" # Subnet에 이름 설정
+    }
+}
+
+# Network Interface 리소스 생성
+resource "aws_network_interface" "my_net" {
+    subnet_id   = aws_subnet.my_subnet.id
+    private_ips = ["172.16.10.100"] # Network Interface에 할당할 IP 주소 설정
+    tags = {
+        Name = "private_network_interface" # Network Interface에 이름 설정
+    }
 }
 
 # Ubuntu Instance 생성
 resource "aws_instance" "ubuntu" {
-  ami           = "ami-0225bc2990c54ce9a"  # Ubuntu 20.04 AMI ID 설정
-  instance_type = "t2.micro"  # EC2 Instance 유형 설정
+    ami           = "ami-0225bc2990c54ce9a" # Ubuntu 20.04 AMI ID
+    instance_type = "t2.micro" # 인스턴스 유형 설정
 
-  subnet_id     = aws_subnet.my_subnet.id  # 생성한 Subnet에 인스턴스 배치
-  tags = {
-    Name = "myUbuntu"  # 리소스에 대한 태그 설정
-  }
+    network_interface {
+        network_interface_id = aws_network_interface.my_net.id
+        device_index         = 0
+    }
+    tags = {
+        Name = "myUbuntu" # 인스턴스에 이름 설정
+    }
 }
